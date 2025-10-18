fb1aef7f4d95122cac9785bc7ccd90e0e2f7c7b4
Aditya Das <adityadas99906@gmail.com>
2025-10-18 17:26:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from random import randint
+
+n = randint(1, 100)
+a = 0
+guess = 0
+
+while (a != n):
+    guess = guess + 1
+
+    a = int(input("Guess the number :- "))
+    if (a > n):
+        print("Lower number please")
+    elif (a < n):
+        print("Higher number please")
+    else:
+        break
+    
+print(f"You guessed the number in {guess} attmepts")
